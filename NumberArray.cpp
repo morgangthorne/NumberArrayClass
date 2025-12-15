4ce222a6590912ac7611bf9fd8141bb8b7b6faf4
morgan thorne <morgangthorne@outlook.com>
2025-12-15 17:34:45 -0500

    Created constructor that allows dynamic allocation.

---
+++ NumberArray.cpp
@@ -0,0 +1,20 @@
+#include "NumberArray.h"
+#include <iostream>
+
+using namespace std;
+
+//Costruct
+NumberArray::NumberArray(int s) {
+
+	if (s <= 0)
+		Size = Max_Size;
+	else
+		Size = s;
+
+	Numbers = new double[Size];
+
+	for (int i = 0; i < Size; i++)
+		Numbers[i] = 0.0;
+
+}
+
